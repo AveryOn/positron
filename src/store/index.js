09f938d6974a72010e278ad7dd7893f7dd7c14c9
AveryOn <lnkonion2@gmail.com>
2023-06-08 22:37:19 +0300

    'js'

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    auth: false,
   },
   getters: {
-    returnState(state){
-        console.log(state);
-    }
   },
   mutations: {
   },
